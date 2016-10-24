365260c2b5e5c037df4a7cff888324959c2b7f10
tomerikvalso <tomerikv@yahoo.com>
2016-10-25 01:35:49 +0200

    3x3 matrix with upload

---
+++ demos/text.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+
+		var fileInput = document.getElementById('fileInput');
+		var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+		fileInput.addEventListener('change', function(e) {
+			var file = fileInput.files[0];
+			var textType = /text.*/;
+
+			if (file.type.match(textType)) {
+				var reader = new FileReader();
+
+				reader.onload = function(e) {
+					fileDisplayArea.innerText = reader.result;
+					newBarChart = new THREEGRAPHS.BarChart ( reader.result );
+                    newBarChart.init();
+				}
+
+				reader.readAsText(file);
+			} else {
+				fileDisplayArea.innerText = "File not supported!";
+			}
+		});
+}
