c8f7ca4686b6f71a481edce6593ee1682abdc088
tomerikvalso <tomerikv@yahoo.com>
2016-11-26 20:07:44 +0100

    cleaned up

--- demos/text.js
+++ demos/text.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
 		var fileInput = document.getElementById('fileInput');
-		var fileDisplayArea = document.getElementById('fileDisplayArea');
+		//var fileDisplayArea = document.getElementById('fileDisplayArea');
 
 		fileInput.addEventListener('change', function(e) {
 			var file = fileInput.files[0];
@@ -11,7 +11,7 @@ window.onload = function() {
 				var reader = new FileReader();
 
 				reader.onload = function(e) {
-					fileDisplayArea.innerText = reader.result;
+					//fileDisplayArea.innerText = reader.result;
 					newBarChart = new THREEGRAPHS.BarChart ( reader.result );
                     newBarChart.init();
 				}
